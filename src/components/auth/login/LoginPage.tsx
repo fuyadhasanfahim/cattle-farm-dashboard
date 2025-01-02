e05bdac20346eadb07ce6bb81885fa3db58f7435
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2025-01-02 16:28:34 +0600

    added to vercel

--- src/components/auth/login/LoginPage.tsx
+++ src/components/auth/login/LoginPage.tsx
@@ -1,12 +1,10 @@
-'use client';
-
 import { LoginAction } from '@/actions/user.action';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
-export default function LoginPage() {
+export default async function LoginPage() {
     return (
         <div className="flex flex-col gap-6 w-full max-w-md">
             <Card>
