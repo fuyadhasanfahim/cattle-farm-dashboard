1fde20a5a2705c2a67c8ad3d8b751476dcf1d51d
fuyadhasanfahim <fuyadhasanfahim0@gmail.com>
2025-02-22 15:27:42 +0600

    adding

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -4,7 +4,7 @@ export async function getUserData() {
     try {
         const cookieStore = await cookies();
         const token = cookieStore.get('token')?.value;
-        const res = await fetch('http://localhost:3000/api/auth/profile', {
+        const res = await fetch(`${process.env.DOMAIN}/api/auth/profile`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
