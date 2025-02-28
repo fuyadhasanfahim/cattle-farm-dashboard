80805010798efb683db49bcb4047e0393dc0ff33
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2025-02-28 04:27:39 +0000

    done

--- src/app/(dashboard)/customers/page.tsx
+++ src/app/(dashboard)/customers/page.tsx
@@ -1,6 +1,8 @@
 import DataTable from '@/components/customers/DataTable';
 import HeroSection from '@/components/customers/HeroSection';
 
+export const dynamic = 'force-dynamic';
+
 export default function page() {
     return (
         <>
