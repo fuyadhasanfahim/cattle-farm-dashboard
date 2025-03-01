e786f8cbd2f8539276b3d9fcabd93aea52f99a90
fuyadhasanfahim <fuyadhasanfahim0@gmail.com>
2025-03-01 12:07:03 +0600

    done

---
+++ src/app/(dashboard)/animal-breeding/page.tsx
@@ -0,0 +1,13 @@
+import DataTable from '@/components/manage-cattles/DataTable';
+import HeroSection from '@/components/manage-cattles/HeroSection';
+
+export const dynamic = 'force-dynamic';
+
+export default async function page() {
+    return (
+        <section className="h-full min-h-screen overflow-hidden">
+            <HeroSection />
+            <DataTable />
+        </section>
+    );
+}
---
+++ src/app/(dashboard)/feeding/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from '@/components/feeding/HeroSection';
+
+export default function page() {
+    return (
+        <section>
+            <HeroSection />
+        </section>
+    );
+}
