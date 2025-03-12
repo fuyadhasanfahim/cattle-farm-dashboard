3fd1130e8296888e719d31ef421c83871dba7464
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2025-03-12 06:06:16 +0000

    done adding this in host

--- src/app/api/cattle/get-active-fattening-data/route.ts
+++ src/app/api/cattle/get-active-fattening-data/route.ts
@@ -2,6 +2,8 @@ import dbConfig from '@/lib/dbConfig';
 import CattleModel from '@/models/cattle.model';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
     try {
         await dbConfig();
