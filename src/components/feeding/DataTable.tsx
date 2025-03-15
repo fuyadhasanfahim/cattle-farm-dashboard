38413a0d7e817b657737f9f693fa67e64fc9b9cd
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2025-03-15 11:05:21 +0600

    nothing

--- src/components/feeding/DataTable.tsx
+++ src/components/feeding/DataTable.tsx
@@ -1,35 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { toast } from 'react-hot-toast';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { IFeedingLog } from '@/types/feeding.interface';
 import PurchaseHistory from './PurchaseHistory';
 import FeedingHistory from './FeedingHistory';
 
 export default function DataTable() {
-    const [feedLogs, setFeedLogs] = useState<IFeedingLog[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const res = await fetch('/api/feeding/logs/get-logs');
-                if (!res.ok) throw new Error('Failed to fetch data');
-                const { data } = await res.json();
-
-                setFeedLogs(data);
-            } catch (error) {
-                toast.error(
-                    (error as Error).message || 'Error fetching feed data'
-                );
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, []);
-
     return (
         <section className="mt-6">
             <Tabs defaultValue="PurchaseHistory">
@@ -45,7 +20,7 @@ export default function DataTable() {
                     <PurchaseHistory />
                 </TabsContent>
                 <TabsContent value="FeedingHistory">
-                    <FeedingHistory loading={loading} feedLogs={feedLogs} />
+                    <FeedingHistory />
                 </TabsContent>
             </Tabs>
         </section>
