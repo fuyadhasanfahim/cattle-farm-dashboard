ba3c24464d35e24293a000dde09c0833bb6ef6c0
Fuyad Hasan Fahim <fuyadhasanfahim0@gmail.com>
2025-04-10 13:07:13 +0600

    done

--- src/app/api/expense/seller/add-seller/route.ts
+++ src/app/api/expense/seller/add-seller/route.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import dbConfig from '@/lib/dbConfig';
 import { SellerModel } from '@/models/expense.model';
 import { NextRequest, NextResponse } from 'next/server';
